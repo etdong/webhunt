4be52f4968209d75d26da1604c88c262fad237fc
Eric Dong <eric@donger.ca>
2025-03-08 23:54:52 -0700

    updated menu

--- client/src/scenes/menu.tsx
+++ client/src/scenes/menu.tsx
@@ -2,11 +2,12 @@ import { GameObj, KAPLAYCtx } from "kaplay";
 import debug_Players from "../utils/debug";
 import { getRelativeMousePos, updateCamPos, updateCamZoom } from "../utils/camUtils";
 
+import socket from "src/components/socket";
+
 export default function init_menu(k: KAPLAYCtx) {
-    k.scene('menu', (data) => {
+    k.scene('menu', () => {
 
         // declarations
-        let socket = data.socket;
         let title = 'WEBHUNT';
         let clicked: any = null;
         let letters: GameObj[] = [];
@@ -14,15 +15,28 @@ export default function init_menu(k: KAPLAYCtx) {
         // draw components
         debug_Players(k, socket);
 
-        let start_button = k.add([
-            k.text('start', { size: 64, font: 'gaegu' }),
-            k.pos(k.center()),
-            k.color(0, 0, 0),
-            k.anchor('center'),
+        let background = k.add([
+            k.rect(k.width(), k.height()),
             k.area(),
-            k.scale(1),
-            'start_button'
+            k.anchor('center'),
+            k.pos(k.center()),
         ])
+
+        let menu_buttons = ['rooms list', 'create a room', 'join by code'];
+
+        for (let i = 0; i < menu_buttons.length; i++) {
+            k.add([
+                k.text(menu_buttons[i], { size: 64, font: 'gaegu' }),
+                k.pos(k.width() / 2, k.height() / 2 + i * 128),
+                k.anchor('center'),
+                k.area(),
+                k.color(0, 0, 0),
+                k.scale(1),
+                'menu_button',
+                'button_' + i,
+            ]);
+        }
+
 
         for (let i = 0; i < title.length; i++) {
             let letter = k.add([
@@ -53,53 +67,26 @@ export default function init_menu(k: KAPLAYCtx) {
             ]);
 
             letters.push(letter);
-
-            // need to set the most recent clicked, otherwise you can
-            // accidentally hover another letter and switch targets
-            // eslint-disable-next-line no-loop-func
-            letter.onClick(() => {
-                clicked = letter;
-            })
         }
-
-
-        // socket handling
-        // when a client returns to the menu from game, the game will emit an 'end' event
-        if (data.state === 'end') {
-            socket.emit('end');
-        }
-
-        // received start from server
-        socket.on('enter_lobby', () => {
-            k.go('game', { socket: socket });
-        })
-
-        // 'final_scores' gets emitted after 'end' is received by the server
-        socket.on('final_scores', (data: any) => {
-            let j = 0
-            for (let i in data) {
-                k.add([
-                    k.text(data[i], { size: 32 }),
-                    k.pos(0, 35 + 80 * j),
-                    k.color(255, 0, 255),
-                ]);
-                j += 1;
-            }
-        })
         
         
         // event handlers
-        start_button.onHover(() => {
-            start_button.scale = k.vec2(1.4);
+        k.onClick('', (object) => {
+            if (object.tags.includes('letter')) {
+                clicked = object;
+            } else {
+                clicked = null;
+            }
         })
 
-        start_button.onHoverEnd(() => {
-            start_button.scale = k.vec2(1);
-        })
-
-        start_button.onClick(() => {
-            k.go('game', { socket: socket });
-            socket.emit('signal_start', 4)
+        k.onClick('menu_button', (btn) => {
+            if (btn.text === 'rooms list') {
+                k.go('rooms_list');
+            } else if (btn.text === 'create a room') {
+                k.go('create_room');
+            } else if (btn.text === 'join by code') {
+                k.go('join_room');
+            }
         })
 
         k.onHover('letter', (letter) => {
@@ -119,6 +106,27 @@ export default function init_menu(k: KAPLAYCtx) {
                 k.vec2(0.5),
                 0.1,
                 (newScale) => letter.scale = newScale,
+                k.easings.linear
+            )
+        })
+
+        k.onHover('menu_button', (btn) => {
+            k.tween(
+                btn.scale,
+                k.vec2(1.2),
+                0.1,
+                (newScale) => btn.scale = newScale,
+                k.easings.linear
+            )
+        })
+
+
+        k.onHoverEnd('menu_button', (btn) => {
+            k.tween(
+                btn.scale,
+                k.vec2(1),
+                0.1,
+                (newScale) => btn.scale = newScale,
                 k.easings.linear
             )
         })
@@ -156,7 +164,7 @@ export default function init_menu(k: KAPLAYCtx) {
         })
 
         k.onUpdate(() => {
-            updateCamPos(k, start_button.pos);
+            updateCamPos(k, background.pos);
             updateCamZoom(k);
             for (let i in letters) {
                 if (letters[i].hidden) {
