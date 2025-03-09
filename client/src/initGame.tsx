f0f10857d7193455c94e0f4e6d1854a36d74e99e
Eric Dong <eric@donger.ca>
2025-03-09 03:30:08 -0600

    added create, join, and room list init

--- client/src/initGame.tsx
+++ client/src/initGame.tsx
@@ -1,9 +1,13 @@
 import init_game from "./scenes/game";
 import init_menu from "./scenes/menu";
+import init_create_room from "./scenes/create_room";
 
 import makeKaplayCtx from "./kaplayCtx";
 
-import socket from 'src/components/socket';
+import socket from "src/components/socket";
+import init_room from "./scenes/room";
+import init_join_room from "./scenes/join_room";
+import init_rooms_list from "./scenes/rooms_list";
 
 export default async function initGame() {
 	const k = makeKaplayCtx()
@@ -21,7 +25,10 @@ export default async function initGame() {
     // scenes
     init_menu(k)
     init_game(k)
+    init_room(k)
+    init_rooms_list(k)
+    init_join_room(k)
+    init_create_room(k)
 
-    let data = {socket: socket}
-	k.go('menu', data)
+	k.go('rooms_list')
 }
