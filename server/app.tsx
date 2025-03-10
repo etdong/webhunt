77225151ad86e21380510616f81d15be11e6db09
Eric Dong <eric@donger.ca>
2025-03-10 15:57:53 -0600

    login checking debug

--- server/app.tsx
+++ server/app.tsx
@@ -299,6 +299,7 @@ io.sockets.on('connection', (socket: any) => {
     })
 
     socket.on('check_login', (socketId: string, callback: any) => {
+        console.log('checking login for %s', socketId)
         let player = player_list[socketId];
         if (player === undefined || player.googleId === "") callback(false);
         callback(true);
